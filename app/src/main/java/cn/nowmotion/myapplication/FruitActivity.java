bd4fcf3981f2faf2a9cf05682e526576c680439c
kingone1995 <124530521+kingone1995@users.noreply.github.com>
2024-05-18 03:18:03 +0800

    测试
    测试版

--- app/src/main/java/cn/nowmotion/myapplication/FruitActivity.java
+++ app/src/main/java/cn/nowmotion/myapplication/FruitActivity.java
@@ -39,8 +39,7 @@ public class FruitActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-
+        System.out.println("测试内容");
 
         Custon_Status_bar.全透明(this);
         Custon_Status_bar.白底黑字(this);
