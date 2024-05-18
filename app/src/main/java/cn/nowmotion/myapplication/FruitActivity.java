c0419e959c6d78e2377486bd65226ccc74ad8ff0
kingone1995 <124530521+kingone1995@users.noreply.github.com>
2024-05-18 09:43:57 +0800

    rr
    rr

--- app/src/main/java/cn/nowmotion/myapplication/FruitActivity.java
+++ app/src/main/java/cn/nowmotion/myapplication/FruitActivity.java
@@ -39,51 +39,5 @@ public class FruitActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        collapsing = findViewById(R.id.collapsing);
-        toolbar = findViewById(R.id.toolbar);
-//        floating = findViewById(R.id.floating);
-        tv_text = findViewById(R.id.tv_text);
-
-        setSupportActionBar(toolbar);
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.setDisplayHomeAsUpEnabled(true);
-        }
-        collapsing.setTitle("这是CollapsingToolbarLayout");
-        String text = "努力努力再努力";
-
-
-        tv_text.setText(generateText(text));
-//        floating.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Toast.makeText(FruitActivity.this, "您点击了悬浮按钮哦！", Toast.LENGTH_SHORT).show();
-//            }
-//        });
-
-
-    }
-
-
-    private String generateText(String text) {
-        StringBuilder stringBuilder = new StringBuilder("");
-        for (int i = 0; i < 500; i++) {
-            stringBuilder.append(text);
-        }
-        return stringBuilder.toString();
-    }
-
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()) {
-            //Toolbar左上角默认有一个返回的箭头，含义是返回上一个活动
-            //这个按钮叫做HomeAsUp按钮，这个按钮的id永远都是android.R.id.home
-            case android.R.id.home:
-                finish();
-                break;
-        }
-        return true;
     }
 }
